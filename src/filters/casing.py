27e31f4119bc0ad80e4d8f17b44b97aac6af4142
Jordan Barnes <rjordanbarnes@gmail.com>
2012-09-05 21:27:38 -0700

    Add prerequisite functions to Casing.

--- src/filters/casing.py
+++ src/filters/casing.py
@@ -5,3 +5,9 @@ class Casing(wx.Panel):
     def __init__(self, parent, files):
         wx.Panel.__init__(self, parent)
         wx.StaticText(self, -1, "Change Casing of text", (60, 60))
+
+    def refresh(self, previews):
+        pass
+
+    def cleanUpTab(self):
+        pass
